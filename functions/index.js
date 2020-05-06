e06dc71a669c9f9cbef3c40c7c81ef9b4f501c1b
Abhishek <abhishek7gg7@gmail.com>
2020-05-06 17:42:41 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 exports.api = functions.https.onRequest((request, response) => {
  response.send("Hello World");
