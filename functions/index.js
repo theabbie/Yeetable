6f122bbe51b4a1f6d9dabb80c4a7d9e682a2a6f5
Abhishek <abhishek7gg7@gmail.com>
2020-05-06 17:54:48 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,6 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.api = functions.https.onRequest((request, response) => {
- response.send("Hello World");
+exports.api = functions.https.onRequest((req, res) => {
+ admin.messaging().subscribeToTopic([req.query.token], "push").then(function(x) {res.send("")}).catch(function(error) {res.send("error")});
 });
