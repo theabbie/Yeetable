025d362ee395dc2f376b0f39de2a81f8b4c06875
Abhishek <abhishek7gg7@gmail.com>
2020-06-20 01:31:09 +0530

    [skip ci]

--- functions/index.js
+++ functions/index.js
@@ -142,20 +142,22 @@ return kwds.split(",").map(x=>`<meta property="article:tag" content="${x.trim()}
 <meta name="twitter:description" content="${desc}" />
 <meta name="twitter:title" content="${title}" />
 <meta name="twitter:image" content="https://theabbie.github.io/files/collage.jpg" />
+<!-- 
 <link rel="manifest" href="/manifest.json">
 <link rel="alternate" type="application/rss+xml" title="TheAbbie Blog" href="https://theabbie.github.io/rss.xml" />
+-->
 <link rel="me" href="mailto:abhishek7gg7@gmail.com" />
 <link rel="me" href="sms:+918928412138" />
-<link rel="apple-touch-icon" sizes="180x180" href="/files/favicons/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/files/favicons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="194x194" href="/files/favicons/favicon-194x194.png">
-<link rel="icon" type="image/png" sizes="192x192" href="/files/favicons/android-chrome-192x192.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/files/favicons/favicon-16x16.png">
-<link rel="mask-icon" href="/files/favicons/safari-pinned-tab.svg" color="#000000">
-<link rel="shortcut icon" href="/files/favicons/favicon.ico">
+<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="194x194" href="/favicons/favicon-194x194.png">
+<link rel="icon" type="image/png" sizes="192x192" href="/favicons/android-chrome-192x192.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000">
+<link rel="shortcut icon" href="/favicons/favicon.ico">
 <meta name="msapplication-TileColor" content="#000000">
-<meta name="msapplication-TileImage" content="/files/favicons/mstile-144x144.png">
-<meta name="msapplication-config" content="/files/favicons/browserconfig.xml">
+<meta name="msapplication-TileImage" content="/favicons/mstile-144x144.png">
+<meta name="msapplication-config" content="/favicons/browserconfig.xml">
 <meta name="theme-color" content="#ffffff">
 <style amp-custom>
 @font-face {
