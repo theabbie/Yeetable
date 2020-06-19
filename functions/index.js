7247ba93d05bcd2ab473611647f6c3146677b312
Abhishek <abhishek7gg7@gmail.com>
2020-06-20 01:08:56 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -57,7 +57,7 @@ var title = decodeURI(req.url.substring(1)).split("-").join(" "); //make_title(1
 var kwds = title.split(" ").join(",");
 
 var map = {" ": 1,"a": 1,"b": 2,"c": 3,"d": 4,"e": 5,"f": 6,"g": 7,"h": 8,"i": 9,"j": 10,"k": 11,"l": 12,"m": 13,"n": 14,"o": 15,"p": 16,"q": 17,"r": 18,"s": 19,"t": 20,"u": 21,"v": 22,"w": 23,"x": 24,"y": 25,"z": 26,0:1,1:1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9};
-var Math.seed = 0;
+Math.seed = 0;
 title.split("").forEach((x,i) => {
   Math.seed += (map[x] || 0)*(i+1);
 });
