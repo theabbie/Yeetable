469bfbc5374fe0c2c596b0d1e904c5dfe5f35d58
Abhishek <abhishek7gg7@gmail.com>
2020-06-20 13:47:14 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const axios = require('axios');
+const url = require('url');
 admin.initializeApp();
 
 exports.subscribe = functions.https.onRequest((req, res) => {
@@ -52,8 +53,9 @@ exports.save = functions.https.onCall((data, context) => {
 
 exports.markov = functions.https.onRequest((req, res) => {
 
-var turl = req.url;
-var title = decodeURI(req.url.substring(1)).split("-").join(" "); //make_title(12);
+var u = url.parse(req.url,true);
+var turl = u.pathname;
+var title = decodeURI(turl.substring(1)).split("-").join(" "); //make_title(12);
 var kwds = title.split(" ").join(",");
 
 var map = {" ": 1,"a": 1,"b": 2,"c": 3,"d": 4,"e": 5,"f": 6,"g": 7,"h": 8,"i": 9,"j": 10,"k": 11,"l": 12,"m": 13,"n": 14,"o": 15,"p": 16,"q": 17,"r": 18,"s": 19,"t": 20,"u": 21,"v": 22,"w": 23,"x": 24,"y": 25,"z": 26,0:1,1:1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9};
